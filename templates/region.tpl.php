492f793cd829d4f98d54131966e4b6cbb20d0f08
Eric Rasmussen <ericrasmussen1@gmail.com>
2018-12-10 10:34:03 -0600

    Restore wrappers for content region for use with 5.0 grid (so that regions are all wrapped)

--- templates/region.tpl.php
+++ templates/region.tpl.php
@@ -1,17 +1,13 @@
 <?php
 
 $wrapper_id = 'id="' . $region_name . '"';
-  
+
 if ($format == 'partial') {
   //don't give an id to the wrapper for partial requests.  This can result in duplicate ID errors.
   $wrapper_id = '';
 }
 ?>
 
-<?php if ($region != 'navlinks' && $region != 'content'): ?>
 <div <?php print $wrapper_id; ?> class="<?php print $classes; ?>">
-<?php endif;?>
   <?php print $content; ?>
-<?php if ($region != 'navlinks' && $region != 'content'): ?>
 </div>
-<?php endif;?>
