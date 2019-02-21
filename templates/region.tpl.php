068c81233558066d539ba93b4c3e7483778bde98
Eric Rasmussen <ericrasmussen1@gmail.com>
2019-02-21 14:21:30 -0600

    Revert

--- templates/region.tpl.php
+++ templates/region.tpl.php
@@ -1,14 +1,12 @@
 <?php
 
 $wrapper_id = 'id="' . $region_name . '"';
-$format = filter_input(INPUT_GET, 'format', FILTER_SANITIZE_STRING);
 
 if ($format == 'partial') {
   //don't give an id to the wrapper for partial requests.  This can result in duplicate ID errors.
   $wrapper_id = '';
 }
 ?>
-
 <div <?php print $wrapper_id; ?> class="<?php print $classes; ?>">
   <?php print $content; ?>
 </div>
