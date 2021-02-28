594317ff328296041160b964959d951ac8fd9afd
Eric Rasmussen <ericrasmussen1@gmail.com>
2021-02-28 17:11:06 -0600

    Update to 5.3

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,6 +1,6 @@
 <?php
 function callback($buffer) {
-  return (str_replace("templates_5.2/", "templates_5.2_darkmode/", $buffer));
+  return (str_replace("templates_5.3/", "templates_5.2_darkmode/", $buffer));
 }
 if (theme_get_setting('unl_darkmode_preview') && count(array_intersect(array('Site Admin'), array_values($GLOBALS['user']->roles)))) {
   ob_start("callback");
@@ -9,7 +9,7 @@ if (theme_get_setting('unl_darkmode_preview') && count(array_intersect(array('Si
 <?php if ($format !== 'partial'): ?><!DOCTYPE html>
 <html class="dcf-no-js dcf-no-webp" lang="<?php print $language->language; ?>"><!-- InstanceBegin template="/Templates/fixed.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
-<?php require(DRUPAL_ROOT."/wdn/templates_5.2/includes/global/head-1.html"); ?>
+<?php require(DRUPAL_ROOT."/wdn/templates_5.3/includes/global/head-1.html"); ?>
 <!--
   Membership and regular participation in the University of Nebraska–Lincoln (UNL) Web Developer Network (WDN) is required to use the UNLedu Web Framework. Visit the WDN site at https://wdn.unl.edu/. Register for our mailing list and add your site or server to UNL Web Audit.
   All framework code is the property of the UNL Web Developer Network. The code seen in a source code view is not, and may not be used as, a template. You may not use this code, a reverse-engineered version of this code, or its associated visual presentation in whole or in part to create a derivative work.
@@ -20,25 +20,24 @@ if (theme_get_setting('unl_darkmode_preview') && count(array_intersect(array('Si
 <!-- InstanceBeginEditable name="doctitle" -->
   <title><?php print $head_title; ?></title>
 <!-- InstanceEndEditable -->
-<?php require(DRUPAL_ROOT."/wdn/templates_5.2/includes/global/head-2.html"); ?>
+<?php require(DRUPAL_ROOT."/wdn/templates_5.3/includes/global/head-2.html"); ?>
 <!-- InstanceBeginEditable name="head" -->
-<link rel="preload" href="https://unlcms.unl.edu/wdn/templates_5.2/css/deprecated.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<noscript><link rel="stylesheet" href="https://unlcms.unl.edu/wdn/templates_5.2/css/deprecated.css"></noscript>
+<link rel="preload" href="https://unlcms.unl.edu/wdn/templates_5.3/css/deprecated.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="https://unlcms.unl.edu/wdn/templates_5.3/css/deprecated.css"></noscript>
 <?php print $head; ?>
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <?php print theme_get_setting('head_html'); ?>
 <!-- InstanceEndEditable -->
 </head>
-<body class="unl <?php print $classes; ?>" data-version="5.2" <?php print $attributes;?>>
-<?php require(DRUPAL_ROOT."/wdn/templates_5.2/includes/global/skip-nav.html"); ?>
+<body class="unl <?php print $classes; ?>" data-version="5.3" <?php print $attributes;?>>
+<?php require(DRUPAL_ROOT."/wdn/templates_5.3/includes/global/skip-nav.html"); ?>
 
 <?php print $page_top; ?>
 <?php endif; ?>
 <?php print $page; ?>
 <?php if ($format !== 'partial'): ?>
-<?php require(DRUPAL_ROOT."/wdn/templates_5.2/includes/global/noscript.html"); ?>
-<?php require(DRUPAL_ROOT."/wdn/templates_5.2/includes/global/js-body.html"); ?>
+<?php require(DRUPAL_ROOT."/wdn/templates_5.3/includes/global/js-body.html"); ?>
 <!-- InstanceBeginEditable name="jsbody" -->
 <?php print $page_bottom; ?>
 <!-- InstanceEndEditable -->
