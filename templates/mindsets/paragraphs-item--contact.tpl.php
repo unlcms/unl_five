f7d74e361eaa4f6c78cdf5d45e6412d168fa3b7a
Eric Rasmussen <ericrasmussen1@gmail.com>
2021-11-16 10:51:07 -0600

    Fix missed twig statement

--- templates/mindsets/paragraphs-item--contact.tpl.php
+++ templates/mindsets/paragraphs-item--contact.tpl.php
@@ -9,7 +9,7 @@
       <h6 class="dcf-mb-0 unl-cream"><?php print render($content['p_n_contact_name']); ?></h6>
       <p class="unl-cream"><?php print render($content['p_n_contact_title']); ?><br>
         <a class="unl-cream" href="tel:+1<?php print str_replace('-', '', $p_n_contact_phone[0]['value']); ?>" aria-label="<?php print implode(' ', str_split(str_replace('-', '.', $p_n_contact_phone[0]['value']))); ?>"><?php print render($content['p_n_contact_phone']); ?></a><br>
-        <a class="unl-cream" href="mailto:{{ (content.p_n_contact_email) }}"><?php print render($content['p_n_contact_email']); ?></a>
+        <a class="unl-cream" href="mailto:<?php print render($content['p_n_contact_email']); ?>"><?php print render($content['p_n_contact_email']); ?></a>
       </p>
     </div>
   </div>
