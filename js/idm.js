fd1008ea01742a726aeff40df00956dedd63364a
Eric Rasmussen <ericrasmussen1@gmail.com>
2022-01-25 17:00:17 -0600

    Update idm.js to work with amd-disable.js and amd-enable.js

--- js/idm.js
+++ js/idm.js
@@ -1,4 +1,15 @@
-require(['idm'], function(idm) {
+(function (drupalSettings) {
+  require(['idm'], function (idm) {
     idm.setLoginURL(drupalSettings.path.baseUrl + 'user/login?destination=' + drupalSettings.path.currentPath);
     idm.setLogoutURL(drupalSettings.path.baseUrl + 'user/logout');
-});
+      
+    // Need to specifically call the render methods because of the use of amd-disable/amd-enable.
+    // idm.setLoginURL() can't be put in the <head> like normal because of the lack of require(),
+    // so by the time it is called above, the UNL theme has already called renderAsLoggedIn().
+    if (drupalSettings.user.uid || idm.isLoggedIn()) {
+      idm.renderAsLoggedIn();
+    } else {
+      idm.renderAsLoggedOut();
+    }
+  });
+})(drupalSettings);
