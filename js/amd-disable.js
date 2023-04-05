31ff9afabc6f3eedfc7963f70db176ba5aba13d9
Eric Rasmussen <ericrasmussen1@gmail.com>
2023-04-05 15:00:44 -0500

    Make define.origAmd available for future js code to use

--- js/amd-disable.js
+++ js/amd-disable.js
@@ -1,1 +1,28 @@
+define.origAmd = define.amd;
 delete define.amd;
+
+/**
+ * Files that need to call WDN.initializePlugin() after this point can
+ * use define.origAmd to restore define.amd before calling and then revert
+ * in an "after" callback function. An example:
+ *
+ *   function initEventsBand() {
+ *     define.amd = define.origAmd;
+ *     delete define.origAmd;
+ *
+ *     var json = $("script[data-selector-json=wdn-events-band-settings]")['0'].innerHTML;
+ *     var json = JSON.parse(json);
+ *     WDN.setPluginParam('events', 'href', json.url);
+ *     WDN.initializePlugin(
+ *         'events-band',
+ *         {limit: json.limit, rooms: true},
+ *         enableAMDcallback,
+ *         'after'
+ *     );
+ *
+ *     function enableAMDcallback() {
+ *       define.origAmd = define.amd;
+ *       delete define.amd;
+ *     }
+ *   }
+ */
