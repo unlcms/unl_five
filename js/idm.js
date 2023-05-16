79f1cf99caf2d9ffa42682ac07429f3cd67e52e4
Eric Rasmussen <ericrasmussen1@gmail.com>
2023-05-16 15:29:31 -0500

    Change the login link to /caslogin

--- js/idm.js
+++ js/idm.js
@@ -1,8 +1,8 @@
 (function (drupalSettings) {
   require(['idm'], function (idm) {
-    idm.setLoginURL(drupalSettings.path.baseUrl + 'user/login?destination=' + drupalSettings.path.currentPath);
+    idm.setLoginURL(drupalSettings.path.baseUrl + 'caslogin?destination=' + drupalSettings.path.currentPath);
     idm.setLogoutURL(drupalSettings.path.baseUrl + 'user/logout');
-      
+
     // Need to specifically call the render methods because of the use of amd-disable/amd-enable.
     // idm.setLoginURL() can't be put in the <head> like normal because of the lack of require(),
     // so by the time it is called above, the UNL theme has already called renderAsLoggedIn().
